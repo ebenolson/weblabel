cba2852e8656fe8ab2fd5ffda97ae0f30a2763fe
Eben Olson <eben.olson@gmail.com>
2015-10-18 16:03:01 -0400

    configure admin page

--- web/docker_django/apps/todo/admin.py
+++ web/docker_django/apps/todo/admin.py
@@ -1,3 +1,10 @@
-from django.contrib import admin
+from django.contrib import admin 
+from models import Image, ImageGroup, Cell, Label, LabelSet, Annotation
 
+admin.site.register(Image)
+admin.site.register(ImageGroup)
+admin.site.register(Cell)
+admin.site.register(Label)
+admin.site.register(LabelSet)
+admin.site.register(Annotation)
 # Register your models here.
