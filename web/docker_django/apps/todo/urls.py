9a1a4e31c41c0145edc6c38149ed277d351e3beb
Eben Olson <eben.olson@gmail.com>
2015-10-18 17:30:38 -0400

    cell image view

--- web/docker_django/apps/todo/urls.py
+++ web/docker_django/apps/todo/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = patterns('',
-    url( r'^$', views.Home.as_view(), name = 'home' ),
-
-    url( r'upload/', views.upload, name = 'jfu_upload' ),
+    url(r'^$', views.Home.as_view(), name='home'),
+    url(r'addimages$', views.UploadImages.as_view(), name='add_images'),
+    url(r'cellimage/(?P<pk>[0-9]+)/$', views.cell_image, name='cell_image'),
+    url(r'upload/', views.upload, name='jfu_upload'),
 )
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- web/docker_django/apps/todo/views.py
+++ web/docker_django/apps/todo/views.py
@@ -1,20 +1,40 @@
 import os
 import hashlib
 import cell_capture
+import PIL.Image
+
 from django.conf import settings
 from django.views import generic
 from django.views.decorators.http import require_POST
+from django.http import HttpResponse
 from jfu.http import upload_receive, UploadResponse
 from .models import Image, Cell
 
 
 class Home(generic.TemplateView):
-    template_name = 'base.html'
+    template_name = 'home.html'
+
+
+class UploadImages(generic.TemplateView):
+    template_name = 'uploadimages.html'
 
     def get_context_data(self, **kwargs):
-        context = super(Home, self).get_context_data(**kwargs)
+        context = super(UploadImages, self).get_context_data(**kwargs)
         context['accepted_mime_types'] = ['image/*']
         return context
+
+
+def cell_image(request, pk):
+    response = HttpResponse(content_type="image/png")
+    try:
+        cell = Cell.objects.get(pk=int(pk))
+        img = PIL.Image.open(cell.image.image.path)
+        img = img.crop((cell.x, cell.y, cell.x+cell.w, cell.y+cell.h))
+    except Image.DoesNotExist:
+        img = Image.new('RGB', (32, 32))
+
+    img.save(response, 'png')
+    return response
 
 
 def find_cells(image):
